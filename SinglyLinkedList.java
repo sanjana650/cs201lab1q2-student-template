5bdb842e1177cf8e078700cc20a84e65be793ffd
sanjana <sanjugundeti@gmail.com>
2026-09-06 00:21:14 +0800

    Cleaned up code

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -125,49 +125,5 @@ public class SinglyLinkedList<E> {
         head = prev;
     }
 
-    public static void main(String[] args) {
-    SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
-    list.addLast(1);
-    list.addLast(2);
-    list.addLast(3);
-    list.addLast(4);
-    list.addLast(5);
-
-    System.out.println("Original: " + list.toString());
-
-    list.reverse();
-    System.out.println("Reversed: " + list.toString());
-
-    System.out.println("First after reverse: " + list.first());
-    System.out.println("Last after reverse: " + list.last());
-    System.out.println("Size: " + list.size());
-
-    //tets
-    System.out.println("=== Single element ===");
-    SinglyLinkedList<Integer> list1 = new SinglyLinkedList<>();
-    list1.addLast(1);
-    System.out.println("Before: " + list1.toString() + " | size=" +
-    list1.size());
-    Integer removed1 = list1.removeLast();
-    System.out.println("Removed: " + removed1);
-    System.out.println("After: " + list1.toString() + " | size=" + list1.size());
-    System.out.println("isEmpty: " + list1.isEmpty());
-    System.out.println();
-
-    // Test 2: removeLast - multiple elements
-    System.out.println("=== Multiple elements ===");
-    SinglyLinkedList<Integer> list2 = new SinglyLinkedList<>();
-    list2.addLast(1);
-    list2.addLast(2);
-    list2.addLast(3);
-    list2.addLast(4);
-    list2.addLast(5);
-    list2.addLast(6);
-    System.out.println("Before: " + list2.toString() + " | size=" +
-    list2.size());
-    Integer removed2 = list2.removeLast();
-    System.out.println("Removed: " + removed2);
-    System.out.println("After: " + list2.toString() + " | size=" + list2.size());
-    System.out.println("last(): " + list2.last());
-    }
+   
 }
